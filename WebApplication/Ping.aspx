2f4f6acecf012f26a3c082b7f2050b4ef2ff0c59
Victor luis <victorlrpf@gmail.com>
2023-11-12 13:38:03 -0300

    finalizacao01

---
+++ WebApplication/Ping.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Ping.aspx.cs" Inherits="WebApplication.ExecutarPing" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="Conteudo" runat="server">
+
+    <div class="margin-top-60">
+        <div class="row">
+            <!-- COMPUTADORES -->
+            <div class="col-6">
+                <asp:Button ID="Executar" OnClick="Executar_Click" runat="server" Text="Executar" />
+                <asp:GridView ID="ExibirComputadores" Width="100%" CellPadding="6" BorderColor="#c0c0c0" runat="server"></asp:GridView>
+            </div>
+            <!--PING -->
+            <div class="col-6">
+                <div class="margin-top-60">
+
+                    <asp:Label ID="Resultado" runat="server"></asp:Label>
+                </div>
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ WebApplication/Ping.aspx.cs
@@ -0,0 +1,61 @@
+﻿using Model;
+using System;
+using System.Net.NetworkInformation;
+
+
+namespace WebApplication
+{
+    public partial class ExecutarPing : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LerComputadores();
+            }
+        }
+
+        protected void LerComputadores()
+        {
+            ExibirComputadores.DataSource = Computadores.ComputadoresAtivos();
+
+            ExibirComputadores.DataBind();
+        }
+
+        protected void Executar_Click(object sender, EventArgs e)
+        {
+            Ping exPing = new Ping();
+
+            int n = ExibirComputadores.Rows.Count;
+
+            for (int i = 0; i < n; i++)
+            {
+                string endereco = ExibirComputadores.Rows[i].Cells[2].Text;
+                string nome = ExibirComputadores.Rows[i].Cells[1].Text;
+
+                try
+                {
+                    PingReply retorno = exPing.Send(endereco);
+
+                    if (retorno.Status == IPStatus.Success)
+                    {
+                        Resultado.Text += nome + "<br/>";
+                        Resultado.Text += retorno.Address.ToString() + "<br/>";
+                        Resultado.Text += "Buffer: " + retorno.Buffer.Length.ToString() + "<br/>";
+                        Resultado.Text += "Time: " + retorno.RoundtripTime.ToString() + "<br/>";
+                        Resultado.Text += "TTL: " + retorno.Options.Ttl.ToString() + "<br/>";
+                        Resultado.Text += "<hr><br/>";
+                    }
+
+                }
+                catch (Exception)
+                {
+                    Resultado.Text = "Falha no Processamento";
+
+                }
+
+            }
+
+        }
+    }
+}
